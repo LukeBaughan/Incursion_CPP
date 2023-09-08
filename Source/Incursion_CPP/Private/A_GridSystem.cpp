9cb7158e31472eab674c6e96dce6f92bfc7041ae
LukeBaughan <43883865+LukeBaughan@users.noreply.github.com>
2023-09-08 21:37:39 +0100

    Grid System Progress
    - Spawns some of the grid nodes in a row
    - Grid System need to always be at 0,0,0 and shouldn't be able to be moved in the editor

--- Source/Incursion_CPP/Private/A_GridSystem.cpp
+++ Source/Incursion_CPP/Private/A_GridSystem.cpp
@@ -3,33 +3,31 @@
 
 #include "A_GridSystem.h"
 #include "A_GridNode.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 AA_GridSystem::AA_GridSystem()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	SceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("DefaultSceenRoot"));
+	SceneComponent->SetupAttachment(RootComponent);
 }
+
+AA_GridSystem::~AA_GridSystem()
+{
+	// Destroys all grid nodes when the AA_GridSystem actor is being destroyed
+	for (AActor* GridNode : AllGridNodes)
+		GridNode->Destroy();
+}
+
 
 // Called when the game starts or when spawned
 void AA_GridSystem::BeginPlay()
 {
 	Super::BeginPlay();
 
-}
-
-void AA_GridSystem::EndPlay(const EEndPlayReason::Type EndPlayReason)
-{
-	// Destroy any child actors when the AA_GridSystem actor is being destroyed
-	TArray<AActor*> ChildActors;
-	GetAttachedActors(ChildActors);
-
-	for (AActor* ChildActor : ChildActors)
-	{
-		ChildActor->Destroy();
-	}
-
-	Super::EndPlay(EndPlayReason);
 }
 
 // Called every frame
@@ -46,32 +44,34 @@ void AA_GridSystem::OnConstruction(const FTransform& Transform)
 	// Calls the parents function
 	Super::OnConstruction(Transform);
 
-	// Creates an array of type AActor pointer
-	//TArray<AActor*> ChildActors;
-	// Stores all of the grid system's child actor pointers in the ChildActor array
-	//GetAttachedActors(ChildActors);
-
 	FTransform SpawnTranform;
 	SpawnTranform.SetLocation(FVector(0.0f, 0.0f, 1.0f));
 
-	AActor* NewChildActor = GetWorld()->SpawnActor<AActor>(AA_GridNode::StaticClass(), SpawnTranform);
-	NewChildActor->AttachToActor(this, FAttachmentTransformRules::KeepWorldTransform);
+	// Attaches any existing grid nodes to the grid system (as when a grid system is placed, its grid nodes lose its parent actor)
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AA_GridNode::StaticClass(), AllGridNodes);
+	for (AActor* GridNode : AllGridNodes)
+		GridNode->AttachToActor(this, FAttachmentTransformRules::KeepRelativeTransform);
 
-	//// Creates the grid
-	//for (int Row = 0; Row < Rows; Row++)
-	//{
-	//	for (int Column = 0; Column < Columns; Column++)
-	//	{
-	//		// Spawns the next child actor, sets it's location and then attaches it to the parent (the Grid system)
-	//		FTransform SpawnTranform;
-	//		SpawnTranform.SetLocation(FVector(Column * 200.0f, Row * 200.0f, 1.0f));
+	CurrentGridSize = AllGridNodes.Num();
+	MaxGridSize = GridRows + 1;
 
-	//		AActor* NewChildActor = GetWorld()->SpawnActor<AActor>(AA_GridNode::StaticClass(), SpawnTranform);
-	//		NewChildActor->AttachToActor(this, FAttachmentTransformRules::KeepWorldTransform);
+	// If there are less grid nodes than the maximum size, add more nodes
+	if (CurrentGridSize < MaxGridSize)
+	{		
+		/*for (int i = 0; i < GridRows; i++)
+		{*/
+			AActor* NewGridNode = GetWorld()->SpawnActor<AActor>(AA_GridNode::StaticClass(), this->GetActorTransform());
+			NewGridNode->AttachToActor(this, FAttachmentTransformRules::KeepRelativeTransform);
+		//}
+	}
 
-	//		UE_LOG(LogTemp, Warning, TEXT("column loop"));
-	//	}
-	//	UE_LOG(LogTemp, Warning, TEXT("row loop"));
-	//}
+	int RowIndex = 0;
+
+	for (AActor * GridNode : AllGridNodes)
+	{
+		float xLocation = RowIndex * 200.0f;
+		GridNode->SetActorLocation(FVector(xLocation, 0.0f, 1.0f));
+		RowIndex++;
+	}
 }
 
--- Source/Incursion_CPP/Public/A_GridSystem.h
+++ Source/Incursion_CPP/Public/A_GridSystem.h
@@ -15,23 +15,27 @@ class INCURSION_CPP_API AA_GridSystem : public AActor
 public:	
 	// Sets default values for this actor's properties
 	AA_GridSystem();
+	~AA_GridSystem();
 
 	UPROPERTY(EditAnywhere, Category = "Grid Size")
-	int Rows = 1;
+	int GridRows = 4;
 	UPROPERTY(EditAnywhere, Category = "Grid Size")
-	int Columns = 1;
+	int GridColumns = 4;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
-		UChildActorComponent* GridNodeComponent;
+private:
+	int CurrentGridSize = 0;
+	int MaxGridSize = 1;
+	TArray<AActor*> AllGridNodes;
+
+	USceneComponent* SceneComponent;
 
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 	// Called when the actor is being destroyed
-	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 	virtual void OnConstruction(const FTransform& Transform) override;
 };
